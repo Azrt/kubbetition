f9582977d01c974c22cc17d674c8c7142936a6a6
Artur Kasprzyk <artur.kasprzyk@fresha.com>
2026-03-08 15:53:54 +0100

    Revert "fix: add guard to games endpoint"
    This reverts commit c48724efccefb70b684ba8990cf72ca8e291f6f0.

--- src/auth/guards/roles.guard.ts
+++ src/auth/guards/roles.guard.ts
@@ -11,11 +11,8 @@ export class RolesGuard implements CanActivate {
   constructor(private reflector: Reflector) {}
 
   canActivate(context: ExecutionContext): boolean | Promise<boolean> {
-    const roles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
-      context.getHandler(),
-      context.getClass(),
-    ]);
-
+    const roles = this.reflector.get(ROLES_KEY, context.getHandler());
+  
     if (!roles) return true;
 
     const request = context.switchToHttp().getRequest();
